b9571e9d4523fbbac139695d056a60f216ff3949
Shant Dashjian <shantgdashjian@gmail.com>
2025-03-05 10:57:48 +0400

    Add endpoint

---
+++ src/main/java/com/polarbookshop/dummyapi/DummyApiApplication.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.dummyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/polarbookshop/dummyapi/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.dummyapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+	@GetMapping("/hello")
+	public String getGreeting() {
+		return "Hello, World!";
+	}
+}
